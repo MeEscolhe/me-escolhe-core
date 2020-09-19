32e72153e0a834b141472333c3f3842941d68ffe
KelvinCL <39067792+KelvinCL@users.noreply.github.com>
2020-09-19 12:13:26 -0300

    Update student.js

--- src/models/student.js
+++ src/models/student.js
@@ -82,35 +82,3 @@ exports.valStudent = validateStudent;
 
  
 
-
-/*
-var userSchema = mongoose.Schema({
-  email: { type: String, required: true, unique: true},
-  password: { type: String, required: true},
-  name: {
-      first: { type: String, required: true, trim: true},
-      last: { type: String, required: true, trim: true}
-  },
-  phone: Number,
-  lists: [listSchema],
-  friends: [{ type : ObjectId, ref: 'User' }],
-  accessToken: { type: String } // Used for Remember Me
-});
-exports.User = mongoose.model('User', userSchema);
-This way you can do this query:
-
-var User = schemas.User;
-User
- .find()
- .populate('friends')
- .exec(...)
-You'll see that each User will have an array of Users (this user's friends).
-
-And the correct way to insert is like Gabor said:
-
-user.friends.push(newFriend._id);*/
-
-
-
-
-
