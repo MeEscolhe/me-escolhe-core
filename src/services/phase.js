a7ddf2275419cce285ebd4e7d76e8ae91bbb02a6
matheusyhwh <matheusyhwh@gmail.com>
2020-10-17 14:03:23 -0300

    inicio de correção do put de selections e phases

--- src/services/phase.js
+++ src/services/phase.js
@@ -43,13 +43,27 @@ router.post("/", async (request, response) => {
   }
 });
 
-router.put("/student/:id", async (req, res) => {
-  PhaseController.addStudent(req.params.id, req.body.studentId)
-    .then((phase) => {
-      res.send(phase);
-    })
-    .catch((error) => res.status(400).send(error));
+router.put("/:id", (request, response) => {
+  const { error, message } = validate(request.body, PhaseController);
+  if (error) {
+    response.status(400).send(message);
+  } else {
+    const propsToUpdate = ["students", "selectionId", "description"];
+    PhaseController.update(
+      request.params.id,
+      filterProps(request.body, propsToUpdate)
+    ).then((phase) => {
+      if (!phase) {
+        response
+          .status(404)
+          .send("The phase with the given ID was not found.");
+      } else {
+        response.send(phase);
+      }
+    });
+  }
 });
+
 router.delete("/:id/student/:studentId", async (req, res) =>
   PhaseController.removeStudent(req.params.id, req.body.studentId)
     .then((phase) => {
