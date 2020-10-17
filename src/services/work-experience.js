fa3f5567ec7ad892e87130c1285639d38f164872
amintasvrp <amintas.pereira@ccc.ufcg.edu.br>
2020-10-17 01:01:36 -0300

    Corrigindo work-xp

--- src/services/work-experience.js
+++ src/services/work-experience.js
@@ -7,17 +7,11 @@ const { isEmpty, validate, filterProps } = require("../middlewares/util");
 
 router
   .route("/")
-  .get((request, response) => 
-  {
-    workExperienceController.getAll().then
-    (
-      (workExperiences) => 
-    {
-      if (isEmpty(workExperiences)) 
-      {
+  .get((request, response) => {
+    workExperienceController.getAll().then((workExperiences) => {
+      if (isEmpty(workExperiences)) {
         response.status(404).send("No work experiences to show.");
-      } else 
-      {
+      } else {
         response.send(workExperiences);
       }
     });
@@ -26,12 +20,9 @@ router
   .post(async (request, response) => {
     const { error, message } = validate(request.body, workExperienceController);
 
-    if (error) 
-    {
+    if (error) {
       response.status(400).send("This work experience cannot be created.");
-    } 
-    else 
-    {
+    } else {
       const workExperience = workExperienceController.create(request.body);
       response.send(workExperience);
     }
@@ -39,83 +30,35 @@ router
 
 router
   .route("/:id")
-  .get(async (request, response) => 
-  {
-    workExperienceController.getById(request.params.id).then
-    (
-      (workExperience) => 
-    {
-      if (!workExperience) 
-      {
-        response.status(404).send("The work experience with the given ID was not found.");
-      } else 
-      {
-        response.send(workExperience);
-      }
-    }
-    );
+  .get(async (request, response) => {
+    workExperienceController
+      .getById(request.params.id)
+      .then((workExperience) => {
+        if (!workExperience) {
+          response
+            .status(404)
+            .send("The work experience with the given ID was not found.");
+        } else {
+          response.send(workExperience);
+        }
+      });
   })
 
-  .delete(async (request, response) => 
-  {
-    workExperienceController.remove(request.params.id).then
-    (
-      (workExperience) => 
-    {
-      if (!workExperience) 
-      {
-        response
-          .status(404)
-          .send("The work experience with the given id was not found.");
-      } 
-      
-      else 
-      {
-        response.send(workExperience);
-      }
-    }
-    );
+  .delete(async (request, response) => {
+    workExperienceController
+      .remove(request.params.id)
+      .then((workExperience) => {
+        if (!workExperience) {
+          response
+            .status(404)
+            .send("The work experience with the given id was not found.");
+        } else {
+          response.send(workExperience);
+        }
+      });
   })
-  // .put((request, response) => 
-  // {
-  //   const id = request.params.id;
-  //   const { error, message } = validate({ id, ...request.body }, workExperienceController);
-  //   if (error) 
-  //   {
-  //     response.status(400).send(message);
-  //   } 
-    
-  //   else 
-  //   {
-  //     const { role, institution, durationInMonths } = request.body;
-
-  //     workExperienceController.update
-  //     (
-  //       request.params.id,
-  //       filterProps({ role, institution, durationInMonths })
-  //     )
-
-  //     .then((workExperience) => 
-  //     {
-  //       if (!workExperience) 
-  //       {
-  //         response
-  //           .status(404)
-  //           .send("The work experience with the given ID was not found.");
-  //       } 
-        
-  //       else 
-  //       {
-  //         response.send(workExperience);
-  //       }
-  //     });
-  //   }
-  // });
   .put((request, response) => {
-    const { error, message } = validate(
-      request.body,
-      workExperienceController
-    );
+    const { error, message } = validate(request.body, workExperienceController);
     if (error) {
       response.status(400).send(message);
     } else {
