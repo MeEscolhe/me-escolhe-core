1ab85df807712c35d2fd6d1098c43792f801bc5e
KelvinCL <39067792+KelvinCL@users.noreply.github.com>
2020-10-18 18:38:04 -0300

    Create lab.js

---
+++ src/doc/controllers/lab.js
@@ -0,0 +1,172 @@
+const withoutParameters = {
+  get: {
+    tags: ["Lab"],
+    description: "",
+    parameters: [],
+    responses: {
+      200: {
+        description: "GET: successful",
+        content: {
+          "application/json": {
+            schema: {
+              type: "array",
+              items: {
+                $ref: "#/components/schemas/lab",
+              },
+            },
+          },
+        },
+      },
+      400: {
+        description: "GET: unsuccessful",
+        content: {
+          "application/json": {
+            schema: {
+              type: "string",
+              example: "No labs to show.",
+              enum: ["No labs to show."],
+            },
+          },
+        },
+      },
+    },
+  },
+  post: {
+    tags: ["Lab"],
+    description: "",
+    parameters: [],
+    requestBody: {
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              "name: ": {
+                type: "string",
+                example: "IQuanta",
+              },
+              "descrition: ": {
+                type: "string",
+                example: "Laboratório de computação quântica",
+              },
+            },
+          },
+        },
+      },
+    },
+    responses: {
+      200: {
+        description: "POST: successful.",
+        content: {
+          "application/json": {
+            schema: {
+              $ref: "#/components/schemas/lab",
+            },
+          },
+        },
+      },
+      400: {
+        description: "GET: unsuccessful",
+        content: {
+          "application/json": {
+            schema: {
+              type: "string",
+              example: "This lab cannot be created.",
+            },
+          },
+        },
+      },
+    },
+  },
+};
+
+const withParameters = {
+  put: {
+    tags: ["Lab"],
+    description: "",
+    parameters: [
+      {
+        name: "id",
+        in: "path",
+        description: "ID of lab",
+        required: true,
+        schema: {
+          type: "string",
+        },
+      },
+    ],
+    requestBody: {
+      content: {
+        "application/json": {
+          schema: {
+            $ref: "#/components/schemas/lab",
+          },
+        },
+      },
+    },
+    responses: {
+      200: {
+        description: "PUT: successful.",
+        content: {
+          "application/json": {
+            schema: {
+              $ref: "#/components/schemas/lab",
+            },
+          },
+        },
+      },
+      400: {
+        description: "PUT: unsuccessful",
+        content: {
+          "application/json": {
+            schema: {
+              type: "string",
+              example: "This lab cannot be created.",
+            },
+          },
+        },
+      },
+    },
+  },
+  delete: {
+    tags: ["Lab"],
+    description: "",
+    parameters: [
+      {
+        in: "path",
+        name: "_id",
+        schema: {
+          type: "string",
+          example: "5f28ac7082e88b35448255e8",
+        },
+        required: true,
+      },
+    ],
+    responses: {
+      200: {
+        description: "DELETE: successful.",
+        content: {
+          "application/json": {
+            schema: {
+              $ref: "#/components/schemas/lab",
+            },
+          },
+          required: true,
+        },
+      },
+
+      404: {
+        description: "DELETE: unsuccessful",
+        content: {
+          "application/json": {
+            schema: {
+              type: "string",
+              example: "The lab with the given id was not found.",
+            },
+          },
+        },
+      },
+    },
+  },
+};
+module.exports = { withParameters, withoutParameters };
