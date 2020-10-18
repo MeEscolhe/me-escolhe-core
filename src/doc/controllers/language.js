a0665ca22cc92eb9b90814c32d4ddc656d93e23c
KelvinCL <39067792+KelvinCL@users.noreply.github.com>
2020-10-18 18:39:19 -0300

    Create language.js

---
+++ src/doc/controllers/language.js
@@ -0,0 +1,172 @@
+const withoutParameters = {
+  get: {
+    tags: ["Language"],
+    description: "",
+    parameters: [],
+    responses: {
+      200: {
+        description: "GET: successful",
+        content: {
+          "application/json": {
+            schema: {
+              type: "array",
+              items: {
+                $ref: "#/components/schemas/language",
+              },
+            },
+          },
+        },
+      },
+      400: {
+        description: "GET: unsuccessful",
+        content: {
+          "application/json": {
+            schema: {
+              type: "string",
+              example: "No languages to show.",
+              enum: ["No languages to show."],
+            },
+          },
+        },
+      },
+    },
+  },
+  post: {
+    tags: ["Language"],
+    description: "",
+    parameters: [],
+    requestBody: {
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              "name: ": {
+                type: "string",
+                example: "Ingles",
+              },
+              "level: ": {
+                type: "number",
+                example: 3,
+              },
+            },
+          },
+        },
+      },
+    },
+    responses: {
+      200: {
+        description: "POST: successful.",
+        content: {
+          "application/json": {
+            schema: {
+              $ref: "#/components/schemas/language",
+            },
+          },
+        },
+      },
+      400: {
+        description: "GET: unsuccessful",
+        content: {
+          "application/json": {
+            schema: {
+              type: "string",
+              example: "This language cannot be created.",
+            },
+          },
+        },
+      },
+    },
+  },
+};
+
+const withParameters = {
+  put: {
+    tags: ["Language"],
+    description: "",
+    parameters: [
+      {
+        name: "id",
+        in: "path",
+        description: "ID of language",
+        required: true,
+        schema: {
+          type: "string",
+        },
+      },
+    ],
+    requestBody: {
+      content: {
+        "application/json": {
+          schema: {
+            $ref: "#/components/schemas/language",
+          },
+        },
+      },
+    },
+    responses: {
+      200: {
+        description: "PUT: successful.",
+        content: {
+          "application/json": {
+            schema: {
+              $ref: "#/components/schemas/language",
+            },
+          },
+        },
+      },
+      400: {
+        description: "PUT: unsuccessful",
+        content: {
+          "application/json": {
+            schema: {
+              type: "string",
+              example: "This language cannot be created.",
+            },
+          },
+        },
+      },
+    },
+  },
+  delete: {
+    tags: ["Language"],
+    description: "",
+    parameters: [
+      {
+        in: "path",
+        name: "_id",
+        schema: {
+          type: "string",
+          example: "5f28ac7082e88b35448255e8",
+        },
+        required: true,
+      },
+    ],
+    responses: {
+      200: {
+        description: "DELETE: successful.",
+        content: {
+          "application/json": {
+            schema: {
+              $ref: "#/components/schemas/language",
+            },
+          },
+          required: true,
+        },
+      },
+
+      404: {
+        description: "DELETE: unsuccessful",
+        content: {
+          "application/json": {
+            schema: {
+              type: "string",
+              example: "The language with the given id was not found.",
+            },
+          },
+        },
+      },
+    },
+  },
+};
+module.exports = { withParameters, withoutParameters };
