c0a8b91904cc0f0fb07bfbc6bc5849e6e2d5f576
amintasvrp <amintas.pereira@ccc.ufcg.edu.br>
2021-03-13 03:59:14 -0300

    eu n aguento mais

--- src/controllers/selection.js
+++ src/controllers/selection.js
@@ -45,17 +45,18 @@ const getAll = async ({ page = DefaultPage, limit = DefaultPageLimit }) => {
  */
 const getFullById = async (id) => {
   let selection = await MongoDb.getById(Selection, id);
-  if (selection)
+  if (selection) {
     selection = await overrideAttribute(
       selection,
       "projectId",
       "project",
       await ProjectController.getById(selection.projectId)
     );
-  selection.students = await MongoDb.getByRegistrations(
-    Student,
-    selection.students
-  );
+    selection.students = await MongoDb.getByRegistrations(
+      Student,
+      selection.students
+    );
+  }
   return selection;
 };
 
